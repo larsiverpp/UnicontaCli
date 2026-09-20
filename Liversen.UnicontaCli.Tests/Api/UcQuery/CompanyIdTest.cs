286648553d9eb8b9aaed50826be2bb00d7de4c0a
Lars Iversen <LAIVE@danskebank.dk>
2026-09-20 17:14:04 +0200

    Add CompanyIdTest

---
+++ Liversen.UnicontaCli.Tests/Api/UcQuery/CompanyIdTest.cs
@@ -0,0 +1,15 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace Liversen.UnicontaCli.Api.UcQuery;
+
+public static class CompanyIdTest
+{
+    [Fact]
+    public static void GivenValue_WhenConstructing_ThenConstructed()
+    {
+        var value = ThreadLocalRandom.Next();
+
+        new CompanyId(value).Value.ShouldBe(value);
+    }
+}
